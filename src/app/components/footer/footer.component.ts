b4e49ae4c3bfe03e4284e54ec554cf2c39e1df04
Vinicius Monteiro <vinicivs.monteiro@gmail.com>
2021-07-17 10:50:49 -0300

    footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,27 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { ABOUT, COPYRIGHT, SOCIAL, SUPPORT, TECH } from './data';
+import { ContentService } from 'src/app/services/content.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.sass']
 })
-export class FooterComponent implements OnInit {
-  support$: Observable<any> = of(SUPPORT);
-  tech$: Observable<any> = of(TECH);
-  about$: Observable<any> = of(ABOUT)
-  social$: Observable<any> = of(SOCIAL)
-  copyright$: Observable<any> = of(COPYRIGHT)
-  sections: Observable<any[]> = of([
-    SUPPORT,
-    TECH,
-    ABOUT
-  ])
+export class FooterComponent  implements OnInit {
+  copyright$: Observable<any> = of(null)
+  content$: Observable<any> = of(null)
 
-  constructor() { }
+  constructor(private contentService: ContentService) { }
 
   ngOnInit(): void {
+    this.content$ = this.contentService.find('footer');
+    this.copyright$ = this.contentService.find('copyright');
   }
-
 }
