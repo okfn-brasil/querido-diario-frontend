da0d66bc05643400ba12b1866665be176c31607e
Vinicius Monteiro <vinicivs.monteiro@gmail.com>
2021-07-18 21:40:31 -0300

    nee content

--- src/app/data/levels.ts
+++ src/app/data/levels.ts
@@ -13,6 +13,7 @@ interface Action {
 }
 export interface Level {
   icon: string;
+  title: string;
   texts?: string[];
   descriptions?: string[];
   actions?: Action[]
@@ -21,6 +22,7 @@ export interface Level {
 const LEVELS: Level[] = [
   {
     icon: 'level-1',
+    title: 'Nível 0 de acesso',
     descriptions: [
       'Esta cidade ainda não está no Querido Diário. A plataforma é colaborativa e precisamos ter a fonte de publicação desse município. Você pode nos ajudar?',
     ],
@@ -34,6 +36,7 @@ const LEVELS: Level[] = [
   },
   {
     icon: 'level-1',
+    title: 'Nível 1 de acesso',
     descriptions: [
       'O Querido Diário possui a fonte de publicação do diário oficial desta cidade, mas ainda não consegue coletar os arquivos. <a href=\`https://www.catarse.me/queridodiario-okbr\`>Doe</a> e nos ajude a disponibilizar informações de mais cidades na plataforma.',
     ],
@@ -43,17 +46,21 @@ const LEVELS: Level[] = [
   },
   {
     icon: 'level-1',
+    title: 'Nível 2 de acesso',
     texts: [
       'O Querido Diário encontrou a cidade, mas ainda não é capaz de realizar uma busca por termo. Doe e nos ajude a disponibilizar informações de mais cidades na plataforma.',
     ],
   },
   {
     icon: 'level-1',
+    title: 'Nível 3 de acesso',
     descriptions: [
       'Conseguimos disponibilizar os diários deste município em formato aberto! Assim você consegue realizar a busca nos documentos de um jeito fácil.',
     ],
   },
 ];
+
+export const LEVELS_FOR_PAGE = LEVELS
 
 export function findLevel(level: number): Level {
   return LEVELS[level]
