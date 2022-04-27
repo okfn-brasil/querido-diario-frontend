8784498a1d097e53a3afc2c9f8ad104f3f8beb7d
ArianeCamilo <arianecamilo.alves@gmail.com>
2022-04-27 13:05:55 -0300

    Adiciona página de cidades disponíveis

---
+++ src/app/pages/available-cities/available-cities.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { ContentService } from 'src/app/services/content.service';
+
+@Component({
+  selector: 'app-available-cities',
+  templateUrl: './available-cities.component.html',
+  styleUrls: ['./available-cities.component.sass']
+})
+export class AvailableCitiesComponent implements OnInit {
+  content$: Observable<any> = of(null);
+
+  constructor(private contentService: ContentService) {}
+
+  ngOnInit(): void {
+    this.content$ = this.contentService.find('available-cities');  
+  }
+
+}
+
