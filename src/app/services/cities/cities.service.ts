f9495e890c8f82f2d04744188af0d7f11f200918
LucasMendoncaWF <lucasmendoncawf@gmail.com>
2022-08-25 16:19:11 -0300

    constants

--- src/app/services/cities/cities.service.ts
+++ src/app/services/cities/cities.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { API } from 'src/app/constants';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,7 @@ export class CitiesService {
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<any> {
-    return this.http.get(`https://queridodiario.ok.org.br/api/cities?evels=3`).pipe(
+    return this.http.get(`${API}/cities?evels=3`).pipe(
       map((data) => {
         return data;
       })
