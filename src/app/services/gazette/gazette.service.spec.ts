9ed981f9541e6dc34621e225623c177f73d1f491
Lucas Guima <lucasguima.felipe@gmail.com>
2022-10-23 16:56:06 -0300

    Atualiza o teste do serviço Gazette
    Update the Gazette service tests

--- src/app/services/gazette/gazette.service.spec.ts
+++ src/app/services/gazette/gazette.service.spec.ts
@@ -1,13 +1,29 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 import { GazetteService } from './gazette.service';
 
 describe('GazetteService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   let service: GazetteService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
+
+    // Inject the http service and test controller for each test
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+
     service = TestBed.inject(GazetteService);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
