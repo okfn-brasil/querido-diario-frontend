8922836870bf68eb548c65dc81fee87e89b0fc99
LucasMendoncaWF <lucasmendoncawf@gmail.com>
2022-11-07 16:50:00 -0300

    link de relatorios da home com pagina de detalhe

--- src/app/modules/pages/area-education/report-detail/report-detail.component.ts
+++ src/app/modules/pages/area-education/report-detail/report-detail.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-declare const PDFObject: any;
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { ReportItem } from 'src/app/interfaces/reports';
+import { ContentService } from 'src/app/services/content/content.service';
 @Component({
   selector: 'app-report-detail',
   templateUrl: './report-detail.component.html',
   styleUrls: ['./report-detail.component.sass']
 })
 export class ReportDetailComponent implements OnInit {
+  report: ReportItem = {} as ReportItem;
+  file: SafeResourceUrl = {};
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    protected _sanitizer: DomSanitizer,
+    private contentService: ContentService,
+  ) { }
 
   ngOnInit(): void {
-    PDFObject.embed("/pdf/sample-3pp.pdf", "#example1");
+    this.activatedRoute.params.subscribe(params => {
+      const id = parseInt(params.id);
+      
+      this.contentService.find('education-reports').subscribe(result => {
+        this.report = result.reports.find((report: ReportItem) => report.id === id);
+        if(this.report.fileUrl) {
+          this.file = this._sanitizer.bypassSecurityTrustResourceUrl(this.report.fileUrl);
+        }
+      });
+    })
   }
 
 }
