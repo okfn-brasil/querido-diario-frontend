e176cae3dc2f020ae0aed481b1bed8c244208bf2
nkzren <renan.nakazawa0@gmail.com>
2023-10-09 18:43:43 -0300

    [contributors-action] altera contribuição padrão para code,doc

--- .github/actions/contributors/entrypoint.sh
+++ .github/actions/contributors/entrypoint.sh
@@ -5,5 +5,5 @@ export PATH="$(yarn global bin):$PATH"
 all-contributors check --config $GITHUB_WORKSPACE/.all-contributorsrc \
   | sed -n '2p' \
   | sed 's/, /\n/g' \
-  | xargs -I {} all-contributors add --config $GITHUB_WORKSPACE/.all-contributorsrc {} doc
+  | xargs -I {} all-contributors add --config $GITHUB_WORKSPACE/.all-contributorsrc {} code,doc
 
