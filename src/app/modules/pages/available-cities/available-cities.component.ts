9898d7172b668f22dc6d1761c489201282daa084
EdimarioJr <edimariojunior.14@gmail.com>
2024-03-24 01:31:25 -0300

    feat: add cache on cities service and in home

--- src/app/modules/pages/available-cities/available-cities.component.ts
+++ src/app/modules/pages/available-cities/available-cities.component.ts
@@ -7,7 +7,7 @@ import { ContentService } from 'src/app/services/content/content.service';
 @Component({
   selector: 'app-available-cities',
   templateUrl: './available-cities.component.html',
-  styleUrls: ['./available-cities.component.sass']
+  styleUrls: ['./available-cities.component.sass'],
 })
 export class AvailableCitiesComponent implements OnInit {
   content$: Observable<any> = of(null);
@@ -16,18 +16,18 @@ export class AvailableCitiesComponent implements OnInit {
 
   constructor(
     private contentService: ContentService,
-    private citiesService: CitiesService,
-    ) {}
+    private citiesService: CitiesService
+  ) {}
 
   ngOnInit(): void {
     this.content$ = this.contentService.find('available-cities');
 
-    this.citiesService.getAll().subscribe(response => {
+    this.citiesService.getAll().subscribe((response) => {
       this.loading = false;
-      this.cities = response.cities.sort((cityA: City, cityB: City) => 
+
+      this.cities = response.cities.sort((cityA: City, cityB: City) =>
         cityA.territory_name.localeCompare(cityB.territory_name)
       );
     });
   }
 }
-
--- src/app/services/cities/cities.service.ts
+++ src/app/services/cities/cities.service.ts
@@ -1,20 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, shareReplay } from 'rxjs/operators';
 import { API } from 'src/app/constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CitiesService {
+  getAllObservableCache: Observable<any> | null = null;
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<any> {
-    return this.http.get(`${API}/cities?levels=3`).pipe(
-      map((data) => {
-        return data;
-      })
-    );
+    if (!this.getAllObservableCache)
+      this.getAllObservableCache = this.http.get(`${API}/cities?levels=3`).pipe(
+        map((data) => {
+          return data;
+        }),
+        shareReplay(1)
+      );
+
+    return this.getAllObservableCache;
   }
 }
