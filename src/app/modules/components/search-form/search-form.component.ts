be8a882da81492a9141808602bb09eb1b4797eee
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2024-04-11 10:02:17 -0300

    Dowload css
    Corrige bug que não permitia selecionar todos os
    excertos e coloca a div um pouco para a esquerda
    
    Corrigindo bug que não resetava a lista de excertos
    ao fazer nova pesquisa
    
    Co-authored-by: Cristian Furtado <csafurtado.dev@gmail.com>

--- src/app/modules/components/search-form/search-form.component.ts
+++ src/app/modules/components/search-form/search-form.component.ts
@@ -9,6 +9,7 @@ import { Territory } from 'src/app/interfaces/territory';
 import { TerritoryService } from 'src/app/services/territory/territory.service';
 import { Renderer2, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { listGazetteCSV } from '../../pages/search/search.component';
 
 @Component({
   selector: 'app-search-form',
@@ -162,6 +163,7 @@ export class SearchFormComponent implements OnInit {
   }
 
   search(): void {
+    this.clearListGazzete();
     let queryParams = {};
     const term = this.termField.nativeElement.value;
 
@@ -234,4 +236,23 @@ export class SearchFormComponent implements OnInit {
       subscriptions.unsubscribe();
     }
   }
+
+  clearListGazzete(){
+    listGazetteCSV.length = 0
+
+    let buttonDownloadCsv = document.querySelector('.btn-download')
+    let textButtonDownloadCsv = buttonDownloadCsv?.querySelector('strong')
+    let checkFather = document.querySelector('#father')
+
+    if(textButtonDownloadCsv){
+
+      textButtonDownloadCsv.innerText = ``;
+      buttonDownloadCsv?.setAttribute('style', 'background-color: rgba(245, 232, 233, 0.4);')
+
+      let b = checkFather as HTMLInputElement
+
+      b.checked=false
+    }
+  }
+
 }
