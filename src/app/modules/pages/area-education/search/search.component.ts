73d13ce25a692e72f46fe4046d5add4e942cde68
esteerlino <esteerlino@gmail.com>
2024-04-17 11:45:24 -0300

    adicionada lógica para resetar o botão de download após o click e correção de bugs
    Co-authored-by: Raissa Oliveira <raissa.menezesousa@gmail.com>

--- src/app/modules/pages/area-education/search/search.component.ts
+++ src/app/modules/pages/area-education/search/search.component.ts
@@ -11,9 +11,9 @@ interface List {
   [key: number]: GazetteModel[];
 }
 
-interface ValuesCSV{
+interface ValuesCSV {
   Cidade: string,
-  Exerto: string, 
+  Exerto: string,
   Data: string,
   Edicao: string,
   Edicao_Extra: string,
@@ -39,9 +39,9 @@ export class SearchEducationComponent implements OnInit {
   isLoading = false;
   order = OrderFilter.relevance;
   orderList = [
-    {value: OrderFilter.relevance, label: 'Mais relevante'},
-    {value: OrderFilter.descending_date, label: 'Mais recente'},
-    {value: OrderFilter.ascending_date, label: 'Mais antigo'}
+    { value: OrderFilter.relevance, label: 'Mais relevante' },
+    { value: OrderFilter.descending_date, label: 'Mais recente' },
+    { value: OrderFilter.ascending_date, label: 'Mais antigo' }
   ];
   listPageIntern = 0;
   themes: string[] = [];
@@ -77,14 +77,14 @@ export class SearchEducationComponent implements OnInit {
         pre_tags: "<b>",
         post_tags: "</b>",
       } as GazetteFilters;
-      if(Object.keys(Object.keys(this.filters).filter(key => !!this.filters[key])).length > 1) {
+      if (Object.keys(Object.keys(this.filters).filter(key => !!this.filters[key])).length > 1) {
         this.onChangeFilters(this.filters);
       }
     }).unsubscribe();
     this.getFiltersInfo();
 
     this.userQuery.userData$.subscribe(userData => {
-      if(userData.full_name) {
+      if (userData.full_name) {
         this.isLoggedIn = true;
       }
     });
@@ -95,7 +95,7 @@ export class SearchEducationComponent implements OnInit {
   }
 
   onEnter(event?: KeyboardEvent) {
-    if(event && event.key === 'Enter') {
+    if (event && event.key === 'Enter') {
       this.onChangeFilters(this.filters);
     }
   }
@@ -107,13 +107,13 @@ export class SearchEducationComponent implements OnInit {
   getItems(currFilters: string, params?: string) {
     this.isLoading = true;
     this.hasSearched = true;
-    if(this.savedParams !== params) {
+    if (this.savedParams !== params) {
       this.currPage = 0;
     }
 
     this.searchService.getAllGazettes(currFilters, this.currPage).subscribe(response => {
       const nResponse = response as GazetteResponse;
-      if(nResponse.excerpts && nResponse.excerpts.length) {
+      if (nResponse.excerpts && nResponse.excerpts.length) {
         this.results[this.currPage] = parseGazettes(nResponse.excerpts, this.filters.querystring as string);
         this.totalItems = nResponse.total_excerpts;
       } else {
@@ -132,7 +132,7 @@ export class SearchEducationComponent implements OnInit {
   }
 
   getIfCanSearch(obj: GazetteFilters) {
-    if(!!obj.querystring || obj.entities || (obj.subthemes && obj.subthemes.length)) {
+    if (!!obj.querystring || obj.entities || (obj.subthemes && obj.subthemes.length)) {
       return true;
     } else {
       this.hasSearched = false;
@@ -141,54 +141,54 @@ export class SearchEducationComponent implements OnInit {
   };
 
   onChangeFilters(filters: GazetteFilters) {
-    if(this.timeout) {
+    if (this.timeout) {
       clearTimeout(this.timeout)
     }
     this.timeout = setTimeout(() => {
-      this.filters = {...filters, querystring: this.querystring, sort_by: this.order};
+      this.filters = { ...filters, querystring: this.querystring, sort_by: this.order };
       const newObj: GazetteFilters = {} as GazetteFilters;
       Object.keys(this.filters).forEach(key => {
-        if(!!this.filters[key]) {
+        if (!!this.filters[key]) {
           newObj[key] = this.filters[key];
         }
       })
       const params = new URLSearchParams(newObj as any).toString();
-      if(this.getIfCanSearch(newObj)) {
+      if (this.getIfCanSearch(newObj)) {
         this.getItems(this.convertToParams(newObj), params);
       }
-      this.router.navigate([], 
+      this.router.navigate([],
         {
           relativeTo: this.activatedRoute,
           queryParams: newObj,
-      });
+        });
     }, 500);
   }
 
-  
-convertToParams(filters: GazetteFilters){
-  let params = Object.keys(filters)
-  .filter(key => (!!filters[key]))
-  .map(key => {
-    if(Array.isArray(filters[key])) {
-      const arrayItems = filters[key] as string[];
-      const resultArray: string[] = [];
-      arrayItems.forEach(item => {
-        if(item !== '0') {
-          resultArray.push(`${key}=${item}`);
-        }
-      });
-      return resultArray.length ? resultArray.join('&') : '';
-    } else {
-      return `${key}=${filters[key]}`;
-    }
-  })
-  .join('&');
 
-  if(params[params.length - 1] === '&') {
-    params = params.slice(0, -1);
+  convertToParams(filters: GazetteFilters) {
+    let params = Object.keys(filters)
+      .filter(key => (!!filters[key]))
+      .map(key => {
+        if (Array.isArray(filters[key])) {
+          const arrayItems = filters[key] as string[];
+          const resultArray: string[] = [];
+          arrayItems.forEach(item => {
+            if (item !== '0') {
+              resultArray.push(`${key}=${item}`);
+            }
+          });
+          return resultArray.length ? resultArray.join('&') : '';
+        } else {
+          return `${key}=${filters[key]}`;
+        }
+      })
+      .join('&');
+
+    if (params[params.length - 1] === '&') {
+      params = params.slice(0, -1);
+    }
+    return params.replace(/territory_id/g, 'territory_ids');
   }
-  return params.replace(/territory_id/g, 'territory_ids');
-}
 
   getFiltersInfo() {
     this.searchService.getThemes().subscribe(results => {
@@ -211,14 +211,14 @@ convertToParams(filters: GazetteFilters){
   onChangePage($page: number) {
     this.isLoading = true;
     this.currPage = $page;
-    window.scrollTo(0,0);
+    window.scrollTo(0, 0);
     this.onChangeFilters(this.filters);
     const checkbokSelectAll = document.querySelectorAll('.checkbox-all-gazette')
     let c = checkbokSelectAll[0] as HTMLInputElement
     c.checked = false
-    setTimeout(()=>{
+    setTimeout(() => {
       this.validateCheckeds()
-    },1000)
+    }, 1000)
   }
 
   clearFilters() {
@@ -246,102 +246,134 @@ convertToParams(filters: GazetteFilters){
     this.isOpenAdvanced = true;
   }
 
-  addValuesCsv(territory_name:string, 
-    excerpt:string, 
-    date:string, 
-    edition:string, 
-    is_extra_edition:boolean, 
-    txt_url:string, 
-    pdf_url:string
+  addValuesCsv(territory_name: string,
+    excerpt: string,
+    date: string,
+    edition: string,
+    is_extra_edition: boolean,
+    txt_url: string,
+    pdf_url: string
   ) {
 
-    let val:ValuesCSV= {
-    Cidade: territory_name,
-    Exerto: excerpt, 
-    Data: date,
-    Edicao: edition,
-    Edicao_Extra: is_extra_edition ? "Edicao extra":"Não extra",
-    URL_Texto: txt_url,
-    URL_PDF: pdf_url
+    let val: ValuesCSV = {
+      Cidade: territory_name,
+      Exerto: excerpt,
+      Data: date,
+      Edicao: edition,
+      Edicao_Extra: is_extra_edition ? "Edicao extra" : "Não extra",
+      URL_Texto: txt_url,
+      URL_PDF: pdf_url
     }
 
     const checkbokSelectAll = document.querySelectorAll('.checkbox-all-gazette')
     let cAll = checkbokSelectAll[0] as HTMLInputElement
 
-    for(let i=0; i<this.valuesCsv.length; i++){
-      if(this.valuesCsv[i].Exerto == val.Exerto){
-        this.valuesCsv.splice(i,1)
+    for (let i = 0; i < this.valuesCsv.length; i++) {
+      if (this.valuesCsv[i].Exerto == val.Exerto) {
+        this.valuesCsv.splice(i, 1)
         cAll.checked = false
         return
       }
     }
-    
+
     this.valuesCsv.push(val)
     this.isAtLeastOneSelected = this.valuesCsv.length > 0
+    this.checkDownloadButton()
   }
 
-  downloadCSV(){
+  downloadCSV() {
     if (!this.isAtLeastOneSelected) {
-      return;
+      return
     }
-    var options = { 
+    var options = {
       fieldSeparator: ',',
       quoteStrings: '"',
       decimalseparator: '.',
-      showLabels: true, 
+      showLabels: true,
       useBom: true,
       noDownload: false,
       headers: ["Cidade", "Exerto", "Data", "Edicao", "Edicao_Extra", "URL_Texto", "URL_PDF"]
     };
-    new ngxCsv(this.valuesCsv, "pesquisa", options);
+    new ngxCsv(this.valuesCsv, "pesquisa", options)
     this.valuesCsv = []
+    this.resetStateAfterDownload()
   }
 
-  selectAllGazette(){
+  selectAllGazette() {
     const checkbokSelectAll = document.querySelectorAll('.checkbox-all-gazette')
-    const checkboxes_gazettes = document.querySelectorAll('input[name="checkbox-gazette"]');
-    
+    const checkboxes_gazettes = document.querySelectorAll('input[name="checkbox-gazette"]')
+
     let ca = checkbokSelectAll[0] as HTMLInputElement
 
-    if(ca.checked){
-      for(let i=0; i<checkboxes_gazettes.length; i++){
+    if (ca.checked) {
+      for (let i = 0; i < checkboxes_gazettes.length; i++) {
         let c = checkboxes_gazettes[i] as HTMLInputElement
         let estadoInicial = c.checked
         c.checked = true
-        if(estadoInicial == false){
+        if (estadoInicial == false) {
           c.dispatchEvent(new Event('change'))
         }
       }
-    }else{
-      for(let i=0; i<checkboxes_gazettes.length; i++){
+    } else {
+      for (let i = 0; i < checkboxes_gazettes.length; i++) {
         let c = checkboxes_gazettes[i] as HTMLInputElement
         c.checked = false
         c.dispatchEvent(new Event('change'))
       }
     }
     this.isAtLeastOneSelected = this.valuesCsv.length > 0 || ca.checked
+    this.checkDownloadButton()
   }
 
-  validateCheckeds(){
+  validateCheckeds() {
     const checkbokSelectAll = document.querySelectorAll('.checkbox-all-gazette')
-    const checkboxes_gazettes = document.querySelectorAll('input[name="checkbox-gazette"]');
+    const checkboxes_gazettes = document.querySelectorAll('input[name="checkbox-gazette"]')
     let count = 0
     let cAll = checkbokSelectAll[0] as HTMLInputElement
 
-    for(let i=0; i<checkboxes_gazettes.length; i++){
+    for (let i = 0; i < checkboxes_gazettes.length; i++) {
       let c = checkboxes_gazettes[i] as HTMLInputElement
 
-      this.valuesCsv.map((val, key)=>{
-        if(val.URL_PDF == c.id){
+      this.valuesCsv.map((val, key) => {
+        if (val.URL_PDF == c.id) {
           c.checked = true
-          count +=1
+          count += 1
         }
       })
     }
 
-    if(count==4){
+    if (count == 4) {
       cAll.checked = true
     }
+    this.isAtLeastOneSelected = this.valuesCsv.length > 0 || cAll.checked
+    this.checkDownloadButton()
+  }
+  checkDownloadButton() {
+    const checkboxes_gazettes = document.querySelectorAll('input[name="checkbox-gazette"]')
+    let isEnabled = false
+
+    for (let i = 0; i < checkboxes_gazettes.length; i++) {
+      let c = checkboxes_gazettes[i] as HTMLInputElement
+      if (c.checked) {
+        isEnabled = true
+        return
+      }
+    }
+    const downloadButton = document.querySelector('.btn-download') as HTMLButtonElement
+    downloadButton.disabled = !isEnabled
+  }
+
+  resetStateAfterDownload() {
+    const checkboxes = document.querySelectorAll('input[name="checkbox-gazette"]')
+    checkboxes.forEach((checkbox) => {
+      (checkbox as HTMLInputElement).checked = false
+    });
+    this.isAtLeastOneSelected = false
+
+    const selectAllCheckbox = document.querySelector('.checkbox-all-gazette') as HTMLInputElement
+    selectAllCheckbox.checked = false
+
+    this.checkDownloadButton()
   }
 
 }
