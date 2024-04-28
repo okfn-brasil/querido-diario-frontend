5022ada2428c966ef885f00ef04c1565852c6764
EdimarioJr <edimariojunior.14@gmail.com>
2024-04-28 03:10:38 -0300

    feat: add subtheme, range and entity inputs

--- src/app/modules/pages/area-education/request-analysis-form/request-analysis-form.component.ts
+++ src/app/modules/pages/area-education/request-analysis-form/request-analysis-form.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { City } from 'src/app/interfaces/city';
+import { PeriodRange } from 'src/app/modules/components/period-picker/period-picker.component';
+import { CitiesService } from 'src/app/services/cities/cities.service';
+import { EducationGazettesService } from 'src/app/services/education-gazettes/education-gazettes.service';
 
 @Component({
   selector: 'app-request-analysis-form',
   templateUrl: './request-analysis-form.component.html',
-  styleUrls: ['./request-analysis-form.component.sass']
+  styleUrls: ['./request-analysis-form.component.sass'],
 })
 export class RequestAnalysisFormComponent implements OnInit {
   form: FormGroup = this.formBuilder.group({});
+  cities: City[] = [];
+  themes: string[] = [];
+  locations: string[] = [];
+  entities: string[] = [];
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private citiesService: CitiesService,
+    private searchService: EducationGazettesService
+  ) {}
 
   ngOnInit(): void {
-    this.initForm()
+    this.initForm();
   }
-
 
   initForm() {
     this.form = this.formBuilder.group({
@@ -22,8 +33,39 @@ export class RequestAnalysisFormComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       phone: ['', [Validators.required]],
       keywords: ['', Validators.required],
-      description: ['', Validators.required]
-    })
+      description: ['', Validators.required],
+      city: [[], Validators.required],
+      from: [''],
+      to: [''],
+      entities: [''],
+      subthemes: [''],
+    });
+
+    this.citiesService.getAll().subscribe((cities) => {
+      this.cities = cities.cities as City[];
+    });
+
+    this.searchService.getThemes().subscribe((results) => {
+      this.themes = results as string[];
+    });
   }
 
+  onChangeLocations(locations: string[]) {
+    this.locations = locations;
+    this.form.patchValue({ city: locations });
+  }
+
+  changePeriod(period: PeriodRange) {
+    this.form.patchValue({ from: period.from, to: period.to });
+  }
+
+  onChangeThemes(themes: string[]) {
+    this.themes = themes;
+    this.form.patchValue({ themes });
+  }
+
+  onChangeEntities(entities: string[]) {
+    this.entities = entities;
+    this.form.patchValue({ entities });
+  }
 }
