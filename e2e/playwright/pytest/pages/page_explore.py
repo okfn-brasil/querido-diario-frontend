5eb393f034e9283aed7f6b572a55a5a4d4dee2a0
vitourwd <vitorwilliamd@gmail.com>
2025-01-22 21:55:21 -0300

    primeiro teste aba 'explore'

--- e2e/playwright/pytest/pages/page_explore.py
+++ e2e/playwright/pytest/pages/page_explore.py
@@ -1,20 +1,24 @@
+
+import logging
+import unicodedata
+
 from playwright.sync_api import Page
 
 class ExplorePage:
 
   # Locators
   head_explore     = "explore-head"
-  head_dados       = "dados-head"
-  head_blog        = "blog-head"
-  head_tecnologia  = "tecnologia-head"
-  head_sobre       = "sobre-head"
 
   button_pesquisar    = "btn-pesquisar"
 
   input_conteudo  = "conteudo-campo"
-  input_municipio = "municipio-campo"
+  input_municipio = '[class="location-filter-educacao"]'
   input_inicio_periodo = "data-inicio-periodo"
   input_fim_periodo = "data-fim-periodo"
+
+  dropdown_municipio = '[class="dropdown.ng-star-inserted"]'
+
+  label_resultado = '[class="city-name"]'
 
   # Textos
   TEXT_EXPLORE_PAGE = "Explore diários oficiais municipais..."
@@ -34,19 +38,37 @@ class ExplorePage:
     """
     self.page.get_by_test_id(self.button_pesquisar).click()
 
+  def click_on_municipio_dropdown(self, city_option: str):
+    """
+    Este metodo clica na opcao desejada no dropdown de municipios
+    """
+    # Busca o elemento do dropdown
+    dropdown_class_option = '[class="option has-level ng-star-inserted"]'
+
+    # Formata o unicode do texto (pois contera acentos)
+    normalized_text = unicodedata.normalize("NFC", city_option)
+
+    # Verifica se o dropdown esta visivel
+    self.page.locator(self.dropdown_municipio).is_visible()
+
+    # Clica na opcao do dropdown baseada no texto fornecido
+    self.page.locator(dropdown_class_option).get_by_text(normalized_text).click()
+
   def fill_municipio_input(self, text: str):
     """
     Este metodo insere texto no campo "Municipio"
     :param text: string a ser inserida no campo
     """
-    self.page.get_by_test_id(self.input_municipio).fill(str(text))
+    self.page.get_by_test_id(self.input_conteudo).click()
+    self.page.keyboard.press('Tab')
+    self.page.keyboard.type(text, delay=500)
 
   def fill_conteudo_input(self, text: str):
     """
     Este metodo insere texto no campo "Conteudo (Licitacao, contratacao, etc)"
        :param text: string a ser inserida no campo
     """
-    self.page.get_by_test_id(self.input_municipio).fill(str(text))
+    self.page.get_by_test_id(self.input_conteudo).fill(str(text))
 
   def fill_periodo_input(self, start: str, end: str):
     """
@@ -58,4 +80,5 @@ class ExplorePage:
     """
     self.page.get_by_test_id(self.input_inicio_periodo).fill(str(start))
     self.page.get_by_test_id(self.input_fim_periodo).fill(str(end))
+
 
--- e2e/playwright/pytest/tests/test_explore_page.py
+++ e2e/playwright/pytest/tests/test_explore_page.py
@@ -1,8 +1,12 @@
 import pytest
+import time
+import random
+
 from  playwright.sync_api import Page, expect
 from  e2e.playwright.pytest.pages.page_explore import ExplorePage
+from  e2e.playwright.pytest.util.constants import Constants as c
 
-class TestHomePage:
+class TestExplorePage:
 
   @pytest.fixture(scope="function")
   def explore_page(self, page: Page):
@@ -13,15 +17,48 @@ class TestHomePage:
   def before_after_each(self):
     pass
 
-  def test_validate_explore_header_link(self, explore_page):
+@pytest.mark.TC001
+@pytest.mark.pagina_explore
+def test_busca_diario_oficial_sudeste(self, explore_page):
     """
-    Cenário de Teste:
-      Este teste valida se a página é rediricionada para a rota (url) correta ao clicar no header 'Explore'
-      e se o texto "Explore diários oficiais municipais..." é exibido.
+      Teste ID: TC001
+      Cenário de teste: Fazer uma busca por um diário oficial em um município na região Sudeste do Brasil
+      Passos:
+             1. Acessar a página https://queridodiario.ok.org.br/
+             2. Clicar no botão "Dados"
+             3. No campo de busca digitar "Licitação"
+             4. No campo de Município, digitar um município situado na região Sudeste
+             5. No campo de Período, selecione o período deum ano (retroativo).
+             6. Clique no botão "Pesquisar"
     """
+
+    # municipio = random.choice(c.cidades_por_regiao["Sudeste"])
+    # Dados do Teste
+    municipio_state = "Paulínia (SP)"
+    # Remove qualquer texto após a primeira '(' encontrada na string, para ficar apenas o nome do município
+    municipio = municipio_state.split('(')[0].strip()
+
+    # Clica no botão de aba 'Explore'
     explore_page.click_on_header_explore()
-    expect(explore_page.page).to_have_url('/')
-    expect(explore_page.page.get_by_text(explore_page.TEXT_EXPLORE_PAGE)).to_be_visible()
+
+    # Preenche com dados o campo texto para pesquisa de licitação, diários oficiais, etc
+    explore_page.fill_conteudo_input("Licitação")
+
+    # Preenche o municipio o qual sera pesquisado
+    explore_page.fill_municipio_input(municipio)
+
+    # Seleciona opção no dropdown
+    explore_page.click_on_municipio_dropdown(municipio_state)
+
+    # Preenche o periodo da pesquisa
+    explore_page.fill_periodo_input(start="10/02/2022", end="10/10/2022")
+
+    # Clica no botão 'Pesquisar'
+    explore_page.click_on_pesquisar_button()
+
+    # Verifica se todos os resultados sao do municipio fornecido
+    for locator in explore_page.page.locator(explore_page.label_resultado).all():
+      expect(locator).to_have_text(municipio_state)
 
 
 
