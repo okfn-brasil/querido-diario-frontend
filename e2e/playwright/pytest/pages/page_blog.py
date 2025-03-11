dd894b490d941e44c890db641469c278c066faf1
vitourwd <vitorwilliamd@gmail.com>
2025-03-11 20:19:02 -0300

    arquivo blog criado

---
+++ e2e/playwright/pytest/pages/page_blog.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page, BrowserContext
+import unicodedata
+import logging
+
+class BlogPage:
+
+  link_historia = "História"
+  link_querido_nas_univ = "Querido Diário nas Universidades"
+  link_casos_de_uso = "Casos de Uso"
+  link_comunidade_e_tec = "Comunidade e Tecnologia"
+  link_diario_do_clima = "Diário do Clima"
+  link_tecnologias_educacao = "Tecnologias na Educação"
+
+  def __init__(self, browser : BrowserContext):
+    self.browser = browser
+    self.page = browser.new_page()
