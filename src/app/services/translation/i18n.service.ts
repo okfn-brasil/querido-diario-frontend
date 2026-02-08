d1a79892f648d07c543235adbb052d9c1fb82a41
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2026-02-08 16:24:06 -0300

    Corrige condição de corrida no carregamento de traduções
    O translate.use() era chamado antes das traduções serem carregadas,
    causando erro no ngFor do header e footer que esperavam arrays mas
    recebiam strings literais como 'header.items'.

--- src/app/services/translation/i18n.service.ts
+++ src/app/services/translation/i18n.service.ts
@@ -14,28 +14,21 @@ export class I18nService {
     const routeTranslationPath = `assets/i18n/${language}/${currentRoute}.json`;
     const globalTranslationPath = `assets/i18n/${language}/global.json`;
 
-    this.translate.setTranslation(language, {}, true);
-
     Promise.all([
       fetch(globalTranslationPath).then((res) => res.json()).catch(() => ({})),
       fetch(routeTranslationPath).then((res) => res.json()).catch(() => ({}))
     ])
       .then(([commonTranslations, routeTranslations]) => {
-        this.translate.getTranslation(language).subscribe((existingTranslations) => {
-          const combinedTranslations = {
-            ...existingTranslations,
-            ...commonTranslations,
-            ...routeTranslations
-          };
+        const combinedTranslations = {
+          ...commonTranslations,
+          ...routeTranslations
+        };
 
-
-          this.translate.setTranslation(language, combinedTranslations, true);
-        });
+        this.translate.setTranslation(language, combinedTranslations, true);
+        this.translate.use(language);
       })
       .catch((error) => {
         console.error('Error loading translations:', error);
       });
-
-    this.translate.use(language);
   }
 }
